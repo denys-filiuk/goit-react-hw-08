0e38b315ff3386870b21d0e3ab4aa3adbcec3ede
denys-filiuk <filiuk_dp@ukr.net>
2025-06-11 20:07:21 +0300

    create repository and change back

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://683af87043bb370a86745117.mockapi.io";
+axios.defaults.baseURL = "https://connections-api.goit.global/";
 
 export const fetchContacts = createAsyncThunk("contacts/getAll", async () => {
   const res = await axios.get("/contacts");
