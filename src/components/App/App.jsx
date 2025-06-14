5faced3d6dc11886d05389ac3956e23e7f35ebc9
denys-filiuk <filiuk_dp@ukr.net>
2025-06-14 17:06:40 +0300

    add layout

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,57 +1,50 @@
 import "./App.css";
 import { Toaster } from "react-hot-toast";
-import { Suspense, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { fetchContacts } from "../../redux/contacts/operations";
+import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../../pages/HomePage/HomePage";
 import RegisterPage from "../../pages/RegisterPage/RegisterPage";
 import LoginPage from "../../pages/LoginPage/LoginPage";
 import ContactsPage from "../../pages/ContactsPage/ContactsPage";
-import { AppBar } from "../../components/AppBar/AppBar";
+import Layout from "../../components/Layout/Layout"; // новий імпорт
 import { RestrictedRoute } from "../RestrictedRoute";
 import { PrivateRoute } from "../PrivateRoute";
 
 export default function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
-
   return (
     <>
-      <AppBar />
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
 
-          <Route
-            path="/register"
-            element={
-              <RestrictedRoute redirectTo="/contacts">
-                <RegisterPage />
-              </RestrictedRoute>
-            }
-          />
+            <Route
+              path="register"
+              element={
+                <RestrictedRoute redirectTo="/contacts">
+                  <RegisterPage />
+                </RestrictedRoute>
+              }
+            />
 
-          <Route
-            path="/login"
-            element={
-              <RestrictedRoute redirectTo="/contacts">
-                <LoginPage />
-              </RestrictedRoute>
-            }
-          />
+            <Route
+              path="login"
+              element={
+                <RestrictedRoute redirectTo="/contacts">
+                  <LoginPage />
+                </RestrictedRoute>
+              }
+            />
 
-          <Route
-            path="/contacts"
-            element={
-              <PrivateRoute redirectTo="/login">
-                <ContactsPage />
-              </PrivateRoute>
-            }
-          />
+            <Route
+              path="contacts"
+              element={
+                <PrivateRoute redirectTo="/login">
+                  <ContactsPage />
+                </PrivateRoute>
+              }
+            />
+          </Route>
         </Routes>
       </Suspense>
       <Toaster position="top-right" reverseOrder={false} />
